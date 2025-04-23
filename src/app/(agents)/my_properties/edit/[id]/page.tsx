a4e8996a97f37f1250e247a3e4a8aa353e62a6a3
Linus-Daniel <136229283+Linus-Daniel@users.noreply.github.com>
2025-04-23 08:46:48 +0100

    Updated edit properties

--- src/app/(agents)/my_properties/edit/[id]/page.tsx
+++ src/app/(agents)/my_properties/edit/[id]/page.tsx
@@ -1,4 +1,11 @@
 import EditProperty from "@/components/edit_property";
+import { type Metadata } from 'next';
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  return {
+    title: `Edit Property ${params.id}`,
+  };
+}
 
 export default function Page({ params }: { params: { id: string } }) {
   return <EditProperty id={params.id} />;
