9eb037d69fd143911fed10a4d31bd2d76bd3fea1
Linus-Daniel <vandulinus@gmail.com>
2025-08-19 07:34:18 +0100

    initial commit

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -4,40 +4,25 @@ import ProtectedRoute from "@/components/protected_route";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-  
-      <div
-        className={`antialiased`}
-      >
-    
-        <ProtectedRoute requiredRole="user">
-          <div className="flex h-screen overflow-hidden">
-            {/* Tenant-specific sidebar */}
-            <UserSidebar />
-
-            {/* Main content area */}
-            <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-              {/* Tenant header */}
-              <TenantHeader  />
-
-              {/* Main content */}
-              <main className="flex-1 p-4 md:p-6 bg-white rounded-lg shadow-sm">
-                {children}
-              </main>
-            </div>
+    <div className={`antialiased`}>
+      <ProtectedRoute requiredRole="user">
+        <div className="flex h-screen overflow-hidden">
+          <UserSidebar />
+          <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+            {/* Tenant header */}
+            <TenantHeader />
+            <main className="flex-1 p-4 md:p-6 bg-white rounded-lg shadow-sm">
+              {children}
+            </main>
           </div>
-
-          {/* Toast notifications */}
-          {/* <Toaster /> */}
-        </ProtectedRoute>
-      </div>
+        </div>
+      </ProtectedRoute>
+    </div>
   );
 }
-
